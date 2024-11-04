58a5d2a44000ca3bf21860bbe40c2aba6ab8d4ca
FrankSeg117 <frank.segui1@upr.edu>
2024-11-04 15:58:31 -0400

    Added setters and getters and stadium structures

--- src/ProyectMain/Seat.java
+++ src/ProyectMain/Seat.java
@@ -1,13 +1,35 @@
 package ProyectMain;
 
 public class Seat {
-    public int section;
+    public String section;
     public int row;
     public int seatNumber;
-
-    public Seat (int sec, int row, int seatNumber){
+     
+    //Constructor
+    public Seat (String sec, int r, int sN){
         this.section = sec;
-        this.section = row;
-        this.section = seatNumber;
+        this.row = r;
+        this.seatNumber = sN;
     }
+
+    public String getSection() {
+        return section;
+    }
+    public void setSection(String section) {
+        this.section = section;
+    }
+    public int getRow() {
+        return row;
+    }
+    public void setRow(int row) {
+        this.row = row;
+    }
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+
 }
--- src/ProyectMain/Stadium.java
+++ src/ProyectMain/Stadium.java
@@ -1,5 +1,17 @@
 package ProyectMain;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class Stadium {
     //In progress
+    public Set<Seat> seats = new HashSet<>(); //Set that stores all seats
+    public LinkedList<String> log = new LinkedList<>(); //placeholder, may need to create a class for transactions
+    public HashMap<Client,Seat> Clseats = new HashMap<>(); //Structure to store Client in their seats
+
+
+    
 }
