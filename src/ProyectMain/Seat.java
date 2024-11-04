147a0a79dffb8016f973c71d86ddabb7c72972ca
FrankSeg117 <frank.segui1@upr.edu>
2024-11-04 15:38:01 -0400

    Added seat class with constructor

---
+++ src/ProyectMain/Seat.java
@@ -0,0 +1,13 @@
+package ProyectMain;
+
+public class Seat {
+    public int section;
+    public int row;
+    public int seatNumber;
+
+    public Seat (int sec, int row, int seatNumber){
+        this.section = sec;
+        this.section = row;
+        this.section = seatNumber;
+    }
+}
