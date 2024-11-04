3447116d7b9604b393ac28d82a7e23ab92c2ba67
FrankSeg117 <frank.segui1@upr.edu>
2024-11-04 15:34:08 -0400

    Added client class with constructor

---
+++ src/ProyectMain/Client.java
@@ -0,0 +1,15 @@
+package ProyectMain;
+
+public class Client {
+    public String name;
+    public String email;
+    public String phonenum;
+
+    public Client (String name, String em, String pn){
+        this.name = name;
+        this.email = em;
+        this.phonenum = pn;
+    }
+    
+
+}
